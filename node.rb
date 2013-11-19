f847e920d3d74b78c4172867431ce243ae830286
Mr. Lawrence <lawrence@server.fake>
2013-11-19 20:18:05 +0100

    added tree

---
+++ node.rb
@@ -0,0 +1,14 @@
+require 'ostruct'
+
+class Node < OpenStruct
+  def initialize(parent, tag)
+    super()
+
+    self.parent = parent
+    self.parent.children << self unless parent.nil?
+    self.tag = tag
+    self.children = []
+
+    yield(self) if block_given?
+  end
+end
---
+++ text_adventure.rb
@@ -0,0 +1,25 @@
+require_relative 'node'
+
+Node.new(nil, :root) do|root|
+  Node.new(root, :living_room) do|lr|
+    Node.new(lr, :cat) do|cat|
+      Node.new(cat, :dead_mouse)
+    end
+
+    Node.new(lr, :remote_control) do|rem|
+      Node.new(rem, :dead_batteries)
+    end
+
+    Node.new(lr, :player) do|pl|
+      Node.new(pl, :ham_sandwich)
+    end
+  end
+
+  Node.new(root, :kitchen) do|kit|
+    Node.new(kit, :drawer) do|dr|
+      Node.new(dr, :new_batteries)
+    end
+  end
+
+  Node.new(root, :hall)
+end
