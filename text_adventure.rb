9b8d579f2039ece3c7fd0c7afb29849a4977559a
Mr. Lawrence <lawrence@server.fake>
2013-11-19 23:09:05 +0100

    fixed wrong variable names

--- text_adventure.rb
+++ text_adventure.rb
@@ -2,22 +2,22 @@ require_relative 'node'
 
 Node.new(nil, :root) do |root|
   Node.new(root, :living_room) do |living_room|
-    Node.new(lr, :cat) do |cat|
+    Node.new(living_room, :cat) do |cat|
       Node.new(cat, :dead_mouse)
     end
 
-    Node.new(lr, :remote_control) do |remote_control|
-      Node.new(rem, :dead_batteries)
+    Node.new(living_room, :remote_control) do |remote_control|
+      Node.new(remote_control, :dead_batteries)
     end
 
-    Node.new(lr, :player) do |player|
-      Node.new(pl, :ham_sandwich)
+    Node.new(living_room, :player) do |player|
+      Node.new(player, :ham_sandwich)
     end
   end
 
   Node.new(root, :kitchen) do |kitchen|
-    Node.new(kit, :drawer) do |drawer|
-      Node.new(dr, :new_batteries)
+    Node.new(kitchen, :drawer) do |drawer|
+      Node.new(drawer, :new_batteries)
     end
   end
 
